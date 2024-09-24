083aec609f63fc2f37289dab170b5c8dec4af1f2
yvann-ba <barbot.yvann@gmail.com>
2024-09-24 13:34:19 +0200

    extension

--- src/parsing/check_store_path.c
+++ src/parsing/check_store_path.c
@@ -6,7 +6,7 @@
 /*   By: ybarbot <ybarbot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 10:07:51 by lauger            #+#    #+#             */
-/*   Updated: 2024/09/24 13:28:18 by ybarbot          ###   ########.fr       */
+/*   Updated: 2024/09/24 13:29:14 by ybarbot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ int	check_path(t_read_file *rf, int num_line, char *id)
 	str = ft_substr(line, 2, ft_strlen(line) - 2);
 	if (str == NULL)
 		return (-1);
-	if (has_extenssion(str, ".xpm") == 0)
+	if (has_extension(str, ".xpm") == 0)
 	{
 		free(str);
 		free(line);
