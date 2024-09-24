fa13ca2f4d2f972e6cc63b40c58f7c54bbb34056
lilien86 <lilienauger.86@gmail.com>
2024-09-24 10:46:58 +0200

    fix: remove leak when we exit the program

--- src/init_graphics.c
+++ src/init_graphics.c
@@ -3,42 +3,42 @@
 static void	free_img_mlx_error(t_ray *ray, t_data *data)
 {
 	printf(RED"Error:\nimage paths is incorrect\n"WHITE);
-		if (ray->tx_north->img)
-			mlx_destroy_image(ray->mlx, ray->tx_north->img);
-		if (ray->tx_south->img)
-			mlx_destroy_image(ray->mlx, ray->tx_south->img);
-		if (ray->tx_west->img)
-		mlx_destroy_image(ray->mlx, ray->tx_west->img);
-		if (ray->tx_east->img)
-			mlx_destroy_image(ray->mlx, ray->tx_east->img);
-		if (data->ray->mlx)
-			mlx_destroy_display(data->ray->mlx);
-		if (ray->tx)
-			free(ray->tx);
-		if (ray->tx_north)
-			free(ray->tx_north);
-		if (ray->tx_south)
-			free(ray->tx_south);
-		if (ray->tx_west)
-			free(ray->tx_west);
-		if (ray->tx_east)
-			free(ray->tx_east);
-		free(data->ray->mlx);
-		ft_free_tab_int(data->ray->int_map, data->map_height);
-		free(data->ray);
-		pars_clean_exit(data);
-		exit(0);
+	if (ray->tx_north->img)
+		mlx_destroy_image(ray->mlx, ray->tx_north->img);
+	if (ray->tx_south->img)
+		mlx_destroy_image(ray->mlx, ray->tx_south->img);
+	if (ray->tx_west->img)
+	mlx_destroy_image(ray->mlx, ray->tx_west->img);
+	if (ray->tx_east->img)
+		mlx_destroy_image(ray->mlx, ray->tx_east->img);
+	if (data->ray->mlx)
+		mlx_destroy_display(data->ray->mlx);
+	if (ray->tx)
+		free(ray->tx);
+	if (ray->tx_north)
+		free(ray->tx_north);
+	if (ray->tx_south)
+		free(ray->tx_south);
+	if (ray->tx_west)
+		free(ray->tx_west);
+	if (ray->tx_east)
+		free(ray->tx_east);
+	free(data->ray->mlx);
+	ft_free_tab_int(data->ray->int_map, data->map_height);
+	free(data->ray);
+	pars_clean_exit(data);
+	exit(0);
 }
 
 void	paths_to_mlx_image(t_ray *ray, t_data *data)
 {
-	if (!data)
+	if (!data || !ray)
 		return ;
-	ray->tx = (t_texture *)malloc(sizeof(t_texture));
-	ray->tx_north = (t_texture *)malloc(sizeof(t_texture));
-	ray->tx_south = (t_texture *)malloc(sizeof(t_texture));
-	ray->tx_west = (t_texture *)malloc(sizeof(t_texture));
-	ray->tx_east = (t_texture *)malloc(sizeof(t_texture));
+	ray->tx = (t_texture *)ft_calloc(sizeof(t_texture), 1);
+	ray->tx_north = (t_texture *)ft_calloc(sizeof(t_texture), 1);
+	ray->tx_south = (t_texture *)ft_calloc(sizeof(t_texture), 1);
+	ray->tx_west = (t_texture *)ft_calloc(sizeof(t_texture), 1);
+	ray->tx_east = (t_texture *)ft_calloc(sizeof(t_texture), 1);
 	ray->tx_north->img = mlx_xpm_file_to_image(data->ray->mlx, data->read_file->p_north, &ray->tx_north->width, &ray->tx_north->height);
 	ray->tx_south->img = mlx_xpm_file_to_image(data->ray->mlx, data->read_file->p_south, &ray->tx_south->width, &ray->tx_south->height);
 	ray->tx_west->img = mlx_xpm_file_to_image(data->ray->mlx, data->read_file->p_west, &ray->tx_west->width, &ray->tx_west->height);
@@ -51,8 +51,8 @@ void	paths_to_mlx_image(t_ray *ray, t_data *data)
 	ray->tx_south->addr = mlx_get_data_addr(ray->tx_south->img, &ray->tx_south->bpp, &ray->tx_south->size_line, &ray->tx_south->endian);
 	ray->tx_west->addr = mlx_get_data_addr(ray->tx_west->img, &ray->tx_west->bpp, &ray->tx_west->size_line, &ray->tx_west->endian);
 	ray->tx_east->addr = mlx_get_data_addr(ray->tx_east->img, &ray->tx_east->bpp, &ray->tx_east->size_line, &ray->tx_east->endian);
-
 }
+
 // static void draw_texture(t_ray *ray, t_game *game, t_mlx *mlx, int x)
 // {
 //     int y;
@@ -78,7 +78,7 @@ void	paths_to_mlx_image(t_ray *ray, t_data *data)
 // }
 
 
-void    init_ray(t_ray *ray, t_data *data, int **int_map)
+void	init_ray(t_ray *ray, t_data *data, int **int_map)
 {
 	if (!ray)
 	{
@@ -98,17 +98,37 @@ int	clean_close_windows(void *param)
 	t_data	*data;
 
 	data = (t_data *)param;
-	//free_mlx_images(data);
-	free_img_mlx_error(data->ray, data);
+	
+	if (data->ray->tx_north->img)
+		mlx_destroy_image(data->ray->mlx, data->ray->tx_north->img);
+	if (data->ray->tx_south->img)
+		mlx_destroy_image(data->ray->mlx, data->ray->tx_south->img);
+	if (data->ray->tx_west->img)
+	mlx_destroy_image(data->ray->mlx, data->ray->tx_west->img);
+	if (data->ray->tx_east->img)
+		mlx_destroy_image(data->ray->mlx, data->ray->tx_east->img);
+
+	if (data->ray->tx)
+		free(data->ray->tx);
+
+	if (data->ray->tx_north)
+		free(data->ray->tx_north);
+	if (data->ray->tx_south)
+		free(data->ray->tx_south);
+	if (data->ray->tx_west)
+		free(data->ray->tx_west);
+	if (data->ray->tx_east)
+		free(data->ray->tx_east);
+
 	if (data->ray->img)
 		mlx_destroy_image(data->ray->mlx, data->ray->img);
 	if (data->ray->mlx)
 		mlx_destroy_window(data->ray->mlx, data->ray->mlx_win);
 	if (data->ray->mlx)
 		mlx_destroy_display(data->ray->mlx);
-	//ft_free_tab_int(data->ray->int_map, data->map_height);
-	//free(data->ray->mlx);
-	//free(data->ray);
+	ft_free_tab_int(data->ray->int_map, data->map_height);
+	free(data->ray->mlx);
+	free(data->ray);
 	pars_clean_exit(data);
 	return (0);
 }
